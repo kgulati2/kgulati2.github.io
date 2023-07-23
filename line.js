ab24f5f8e146e364c1c766fd0d8a30a9dea61556
kgulati2 <kgulati2@illinois.edu>
2023-07-23 13:24:53 -0500

    basic graph works

--- line.js
+++ line.js
@@ -2,20 +2,17 @@ function main() {
 
         // set the dimensions and margins of the graph
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    // parse the date / time
-    var parseTime = d3.timeParse("%d-%b-%y");
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
     // set the ranges
-    var x = d3.scaleTime().range([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
+    var xScale = d3.scaleTime().range([0, width]);
+    var yScale = d3.scaleLinear().range([height, 0]);
 
     // define the line
     var valueline = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+    .x(function(d) { return xScale(d.date); })
+    .y(function(d) { return yScale(d.Close); });
 
     // append the svg obgect to the body of the page
     // appends a 'group' element to 'svg'
@@ -28,17 +25,18 @@ function main() {
         "translate(" + margin.left + "," + margin.top + ")");
 
     // Get the data
-    d3.csv("data.csv").then(function(data) {
+    //d3.csv("data.csv").then(function(data) {
+    d3.csv("TSLA.csv").then(function(data) {
 
-    // format the data
+    var parseDate = d3.timeParse("%Y-%m-%d");
     data.forEach(function(d) {
-    d.date = parseTime(d.date);
-    d.close = +d.close;
+        d.date = parseDate(d.Date);
+        d.Close = +d.Close;
     });
 
     // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
+    xScale.domain(d3.extent(data, function(d) { return d.date; }));
+    yScale.domain([0, d3.max(data, function(d) { return d.Close; })]);
 
     // Add the valueline path.
     svg.append("path")
@@ -49,11 +47,12 @@ function main() {
     // Add the x Axis
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(xScale));
 
     // Add the y Axis
     svg.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(yScale));
 
     });
+
 }
